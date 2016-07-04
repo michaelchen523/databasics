5ec4f21049a014504a32f143ccb5b0ab45594a80
Thomas Lilly <tlilly3@gatech.edu>
2016-07-04 13:21:41 +0200

    small updates

--- phase2.sql
+++ phase2.sql
@@ -3,7 +3,7 @@ CREATE TABLE Manager
 (
 Username VARCHAR(255) NOT NULL,
 Email VARCHAR(255) NOT NULL,
-Password VARCHAR(255) NOT NULL,
+MPassword VARCHAR(255) NOT NULL,
 PRIMARY KEY (Username),
 UNIQUE (Email)
 );
@@ -11,12 +11,12 @@ UNIQUE (Email)
 CREATE TABLE Event
 (
 Name VARCHAR(255) NOT NULL,
-Date DATE() NOT NULL,
-StartTime TIME() NOT NULL,
+EDate DATE NOT NULL,
+StartTime TIME NOT NULL,
 Address VARCHAR(255) NOT NULL,
 CityName VARCHAR(255) NOT NULL,
 CountryName VARCHAR(255) NOT NULL,
-EndTime TIME(),
+EndTime TIME,
 Cost INT NOT NULL,
 StudentDiscount TINYINT(1) NOT NULL DEFAULT 0,
 Description TEXT NOT NULL,
@@ -49,9 +49,9 @@ CREATE TABLE Review
 	 Description varchar(255),
 	 ReviewableID int NOT NULL,
 	 PRIMARY KEY(Date, Username),
-	 FOREIGN KEY(Username REFERENCES NormalUser(Username),
-	 FOREIGN KEY(ReviewableID REFERENCES Reviewable(ReviewableID));
-		 ON DELETE RESTRICT		ON UPDATE CASCADE
+	 FOREIGN KEY(Username) REFERENCES NormalUser(Username),
+	 FOREIGN KEY(ReviewableID) REFERENCES Reviewable(ReviewableID)
+		 ON DELETE RESTRICT	ON UPDATE CASCADE);
 
 CREATE TABLE CountryLanguage
 	(CountryName varchar(25) NOT NULL,
@@ -67,8 +67,8 @@ CREATE TABLE CityLanguage
 	 FOREIGN KEY(CityName REFERENCES City(Name));
 
 CREATE TABLE Country
-	(Name VARCAHR(50),
-	NUsername VARCAHR(15) NOT NULL,
+	(Name VARCHAR(50),
+	NUsername VARCHAR(15) NOT NULL,
 	Population INT UNSIGNED NOT NULL,
 
 PRIMARY KEY(Name),
@@ -76,13 +76,13 @@ FOREIGN KEY(NUsername) REFERENCES NormalUser(Username) ON DELETE RESTRICT ON UPD
 );
 
 CREATE TABLE City
-	(Name VARCAHR(50),
-	CountryName VARCAHR(50),
+	(Name VARCHAR(50),
+	CountryName VARCHAR(50),
 	Latitude DOUBLE NOT NULL,
 	Longitude DOUBLE NOT NULL,
-	NUsername VARCAHR(15) NOT NULL,
-	MUsername VARCAHR(15) NOT NULL,
-	ReviewableID INTT NOT NULL,
+	NUsername VARCHAR(15) NOT NULL,
+	MUsername VARCHAR(15) NOT NULL,
+	ReviewableID INT NOT NULL,
 	Capital BOOLEAN NOT NULL,
 
 PRIMARY KEY(Name, CountryName),
